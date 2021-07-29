e7e1573713b5836efbba7a9637b2cd3f3e1a5d4e
Ankur Sheel <ankur.sheel@gmail.com>
2021-07-29 12:48:00 +1200

    get the tokens from secrets

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notion.Client;
+
+namespace ConsoleApp
+{
+    namespace ConsoleApp
+    {
+        public class Program
+        {
+            private static async Task Main()
+            {
+                var config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").AddUserSecrets<Program>().Build();
+
+                var clientOptions = new ClientOptions
+                {
+                    AuthToken = config["Notion:IntegrationToken"]
+                };
+                var weeklyDatabaseId = config["Notion:WeeklyDatabaseId"];
+            }
+        }
+    }
+}
